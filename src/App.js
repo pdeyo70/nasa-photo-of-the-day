500b39e819d46ba96fa267d98323674772aa7a9f
Paul DeYonghe <p.deyonghe@gmail.com>
2019-08-10 10:38:09 -0500

    stuck trying to get API call to work with useEffect()

--- src/App.js
+++ src/App.js
@@ -4,24 +4,23 @@ import axios from "axios";
 import Image from "../src/components/Image";
 import Description from "../src/components/Description";
 
-
-
-
-
-
 function App() {
 
   const [img, setImg] = useState({});
-  
-  
+
+
   useEffect(() => {
+    console.log("useEffect is firing!");
     axios
-      .get(`https://api.nasa.gov/planetary/apod`)
-      .then(res =>  {
+      .get(`https://api.nasa.gov/planetary/apod?api_key=aW8AhwEReGzLfle7n2whLeUzRvNegIcyXJ7Yhtay`)
+      .then(res => {
+        console.log("res: ", res);
         const image = res.data;
+        //console.log("image api: ", image);
         setImg(image);
       })
-  })
+  },
+  [])
 
   console.log(img)
 
@@ -32,7 +31,7 @@ function App() {
       <Image title={img.title} image={img.url} desc={img.explanation} />
       {console.log("img props in App.js: ", img)}
       <Description desc={img.explanation} />
-      
+
     </div>
   );
 }
